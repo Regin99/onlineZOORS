d025899c7a4095820704d6308b9b29f96f03403d
Regin99 <regintmor@gmail.com>
2021-07-26 15:30:11 +0300

    init:start task

---
+++ pages/landing/script.js
@@ -0,0 +1,12 @@
+function range1(value) {
+  let textnumber = document.querySelector(".slider1");
+  textnumber.innerHTML = value * 1 + 1;
+}
+function range2(value) {
+  let textnumber = document.querySelector(".slider2");
+  textnumber.innerHTML = value * 1 + 1;
+}
+function range3(value) {
+  let textnumber = document.querySelector(".slider3");
+  textnumber.innerHTML = value * 1 + 1;
+}
---
+++ pages/map/script.js
@@ -0,0 +1,4 @@
+function range(value) {
+  let textnumber = document.querySelector(".sliderValue");
+  textnumber.innerHTML = value * 1 + 1;
+}
